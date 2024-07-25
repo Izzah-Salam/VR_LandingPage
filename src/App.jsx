cd37053552f4a7b2718cc9c012be323891c3a7c2
izzah <izzah.bscssef20@iba-suk.edu.pk>
2024-07-25 16:51:03 +0500

    start designing VR Technology landing page using tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/components/Header";
+import Hero from "./assets/components/Hero";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white h-screen">
+        <Header />
+        <Hero />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div>
+      <div>VirtualIR</div>
+      <div>
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Workflow</li>
+          <li>Testimonial</li>
+        </ul>
+      </div>
+      <div>
+        <button>Sign In</button>
+        <button>Create an account</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
